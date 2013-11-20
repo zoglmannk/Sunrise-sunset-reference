97c0a3514deb78c24a1afebbc89570198e350436
Kurt Zoglmann <zoglmannk@yahoo.com>
2013-11-19 22:26:44 -0600

    massive readability improvement... For variables that i know the meaning of, I renamed them to something useful. I also got rid of some global variables. Still more to go!

--- src/main/java/sunrise/sunset/App.java
+++ src/main/java/sunrise/sunset/App.java
@@ -4,45 +4,45 @@ public class App {
 
 	private static double[] A = new double[2];
 	private static double[] D = new double[2];
-	
-	private static final double DR = Math.PI/180.0;
-	private static final double K1 = 15.0 * DR * 1.0027379;
-	
-	private static double B5 = 39.185768; //Latitude of Manhattan, KS 
-										  //(north latitudes positive)
-	
-	private static double L5 = -96.575556; //Longitude of Manhattan, KS 
-										   //(west longitudes negative)
-
-	
-	private static double H = 6; // DST offset from UTC (West is positive)
-	
-	private static int YEAR = 2013;
-	private static int MONTH = 11;
-	private static int DAY = 19;
-	
-	
 	private static double Z0;
 	
+	private static final int NEW_STANDARD_EPOC = 2451545; // January 1, 2000 at noon
+	private static final int NUM_DAYS_IN_CENTURY = 36525; // 365 days * 100 years + 25 extra days for leap years
+	
+	private static final double DR = Math.PI/180.0; //degrees to radians constant
+	private static final double K1 = 15.0 * DR * 1.0027379;
+	
+	private static final double LATITUDE  = 39.185768; //Latitude of Manhattan, KS 
+													   //(north latitudes positive)
+	
+	private static final double LONGITUDE = -96.575556; //Longitude of Manhattan, KS 
+														//(west longitudes negative)
+
+	
+	private static final double H = 6; // DST offset from UTC (West is positive)
+	
+	private static final int MONTH = 11;
+	private static final int DAY = 19;
+	private static final int YEAR = 2013;
+	
+	
 	public static void main(String[] args) {
-		L5 = L5/360;
 		Z0 = H/24;
 		
-		calendarToJD();
-		double T = (J-2451545)+F;
-		double TT = T/36525+1; // centuries from 1900.0
+		int julianDate = calendarToJD(MONTH, DAY, YEAR);
+		double daysFromEpoc = (julianDate - NEW_STANDARD_EPOC) + F;
+
 		
-		calculateLST(T); //calculates T0
-		T = T + Z0;
+		double LST = calculateLST(daysFromEpoc, LONGITUDE);
+		daysFromEpoc = daysFromEpoc + Z0;
 		
 		
-		calculateSunPosition(T, TT);
+		calculateSunPosition(daysFromEpoc);
 		A[0] = A5;
 		D[0] = D5;
 		
 		
-		T = T+1;
-		calculateSunPosition(T, TT);
+		calculateSunPosition(daysFromEpoc+1);
 		A[1] = A5;
 		D[1] = D5;
 		
@@ -50,12 +50,12 @@ public class App {
 		if (A[1] < A[0]) {
 			A[1] = A[1]+2*Math.PI;
 		}
-		double Z1 = DR * 90.833; // Zenith dist.
+		double zenithDistance = DR * 90.833;
 		
-		double S = Math.sin(B5*DR);
-		double C = Math.cos(B5*DR);
+		double S = Math.sin(LATITUDE*DR);
+		double C = Math.cos(LATITUDE*DR);
 		
-		double Z = Math.cos(Z1);
+		double Z = Math.cos(zenithDistance);
 		M8 = 0;
 		W8 = 0;
 		
@@ -71,7 +71,7 @@ public class App {
 			double A2 = A[0] + P*DA;
 			double D2 = D[0] + P*DD;
 					
-			testHourForEvent(C0, A0, A2, D2, D0, C, Z, S);
+			testHourForEvent(C0, A0, A2, D2, D0, C, Z, S, LST);
 			
 			A0=A2;
 			D0=D2;
@@ -115,9 +115,9 @@ public class App {
 	private static void testHourForEvent(
 			int C0, double A0, double A2,
 			double D2, double D0, double C, 
-			double Z, double S) {
+			double Z, double S, double LST) {
 		
-		double L0 = T0 + C0*K1;
+		double L0 = LST + C0*K1;
 		double L2 = L0 + K1;
 
 		double H0 = L0 - A0;
@@ -194,38 +194,43 @@ public class App {
 	
 	
 	private static double A5, D5;
-	private static void calculateSunPosition(double T, double TT) {
-		//   Fundamental arguments
-		//     (Van Flandern &
-		//     Pulkkinen, 1979)
-		double L = .779072 + .00273790931*T;
-		double G = .993126 + .0027377785*T;
-
-		L = L - ((int) L);
-		G = G - ((int) G);
-
-		L = L*2*Math.PI;
-		G = G*2*Math.PI;
-
-		double V = .39785 * Math.sin(L);
-		V = V - .01000 * Math.sin(L-G);
-		V = V + .00333 * Math.sin(L+G);
-		V = V - .00021 * TT * Math.sin(L);
+	private static void calculateSunPosition(double daysFromEpoc) {
 		
-		double U = 1 - .03349 * Math.cos(G);
-		U = U - .00014 * Math.cos(2*L);
-		U = U + .00008 * Math.cos(L);
+		double numCenturiesSince1900 = daysFromEpoc/NUM_DAYS_IN_CENTURY + 1;
+		
+		//   Fundamental arguments 
+		//   (Van Flandern & Pulkkinen, 1979)
+		double meanLongitudinal = .779072 + .00273790931*daysFromEpoc;
+		double meanAnomaly      = .993126 + .00273777850*daysFromEpoc;
 
-		double W = -.00010 - .04129 * Math.sin(2*L);
-		W = W + .03211 * Math.sin(G);
-		W = W + .00104 * Math.sin(2*L-G);
-		W = W - .00035 * Math.sin(2*L+G);
-		W = W - .00008 * TT * Math.sin(G);
+		meanLongitudinal = meanLongitudinal - ((int) meanLongitudinal);
+		meanAnomaly      = meanAnomaly      - ((int) meanAnomaly);
+
+		meanLongitudinal = meanLongitudinal * 2*Math.PI;
+		meanAnomaly      = meanAnomaly      * 2*Math.PI;
+
+		double V;
+		V = .39785 * Math.sin(meanLongitudinal);
+		V = V - .01000 * Math.sin(meanLongitudinal-meanAnomaly);
+		V = V + .00333 * Math.sin(meanLongitudinal+meanAnomaly);
+		V = V - .00021 * numCenturiesSince1900 * Math.sin(meanLongitudinal);
+		
+		double U;
+		U = 1 - .03349 * Math.cos(meanAnomaly);
+		U = U - .00014 * Math.cos(2*meanLongitudinal);
+		U = U + .00008 * Math.cos(meanLongitudinal);
+
+		double W;
+		W = -.00010 - .04129 * Math.sin(2*meanLongitudinal);
+		W = W + .03211 * Math.sin(meanAnomaly);
+		W = W + .00104 * Math.sin(2*meanLongitudinal-meanAnomaly);
+		W = W - .00035 * Math.sin(2*meanLongitudinal+meanAnomaly);
+		W = W - .00008 * numCenturiesSince1900 * Math.sin(meanAnomaly);
 		
 		
 		//    Compute Sun's RA and Dec		
 		double S = W / Math.sqrt(U - V*V);
-		A5 = L + Math.atan(S / Math.sqrt(1 - S*S));
+		A5 = meanLongitudinal + Math.atan(S / Math.sqrt(1 - S*S));
 		
 		S = V / Math.sqrt(U);
 		D5 = Math.atan(S / Math.sqrt(1 - S*S));
@@ -236,60 +241,59 @@ public class App {
 	/**
 	 * calculate LST at 0h zone time
 	 */
-	private static double T0;
-	private static void calculateLST(double T) {		
-		T0 = T/36525.0;
+	private static double calculateLST(double daysFromEpoc, double longitude) {
+		double L = longitude/360;
+		double ret = daysFromEpoc/36525.0;
 
 		double S;
-		S = 24110.5 + 8640184.813*T0;
-		S = S + 86636.6*Z0 + 86400*L5;
+		S = 24110.5 + 8640184.813*ret;
+		S = S + 86636.6*Z0 + 86400*L;
 		
 		S = S/86400.0;
 		S = S - ((int) S);
 		
-		T0 = S * 360.0 * DR;
+		ret = S * 360.0 * DR;
 		//System.err.println("T0 at end of calculateLST: "+T0);
+		return ret;
 	}
 	
 	/**
 	 * Compute Julian Date
 	 */
-	private static int J;
 	private static double F;
-	private static void calendarToJD() {
-		int Y = YEAR;
-		int M = MONTH;
-		double D = DAY;
+	private static int calendarToJD(int month, int day, int year) {
+		double D = day;
 		
 		int G=1;
-		if (Y<1583) {
+		if (year<1583) {
 			G=0;
 		}
 		
 		int D1 = (int) D;
 		F = D - ((double) D1) - .5;
 		
-		J = -1 * (int) ( 7 * (((M+9)/12)+Y) / 4);
+		int julianDate = -1 * (int) ( 7 * (((month+9)/12)+year) / 4);
 		
 		
 		int J3 = 0;
 		if(G!=0) {
-			int S = sgn(M-9);
-			int A = Math.abs(M-9);
+			int S = sgn(month-9);
+			int A = Math.abs(month-9);
 			
-			J3 = Y + S * (A/7);
+			J3 = year + S * (A/7);
 			J3 = -1 * ( (J3/100) +1) * 3/4;
 		}
 		
-		J = J + (275*M/9) + D1 + G*J3;
-		J = J + 1721027 + 2*G + 367*Y;
+		julianDate = julianDate + (275*month/9) + D1 + G*J3;
+		julianDate = julianDate + 1721027 + 2*G + 367*year;
 		
 		if (F<0) {
 			F = F+1;
-			J = J-1;
+			julianDate = julianDate-1;
 		}
 		
-		//System.out.println("Julian date: "+J);
+		//System.out.println("Julian date: "+julianDate);
+		return julianDate;
 	}
 
 }
