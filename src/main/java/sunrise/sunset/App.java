85c28ed9fb57cb0d7a3dbfdf47be37024049988c
Kurt Zoglmann <zoglmannk@yahoo.com>
2013-11-26 20:34:49 -0600

    minor change to output and sample calculation date

--- src/main/java/sunrise/sunset/App.java
+++ src/main/java/sunrise/sunset/App.java
@@ -9,19 +9,21 @@ public class App {
 	private static final double LONGITUDE = -96.575556; //Longitude of Manhattan, KS 
 														//(west longitudes negative)
 
-	private static final int    UTC_TO_DST = -6; // DST offset from UTC (West is negative)
+	private static final int    UTC_TO_DST = -5; // DST offset from UTC (West is negative)
 	
 	
 
 	public static void main(String[] args) { 
 		GpsCoordinate gps = new GpsCoordinate(LATITUDE, LONGITUDE);
 		
-		int month=11, day=25, year=2013;
+		int month=11, day=1, year=2013;
+		
 		SimpleDate date = new SimpleDate(month, day, year);
+		System.out.printf("**Calculations for %d/%d/%d****\n", month,day,year);
 		
 		Calculator calculator = new Calculator();
 		Result result = calculator.calculate(gps, UTC_TO_DST, date);
-				
+
 		System.out.println(result.toString());
 	}
 	
